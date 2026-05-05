f3076d83b841e9ae34fe85bc3ee5e9e185b8bf15
rathuco <ahmetcona1963.3@gmail.com>
2026-05-05 04:27:33 +0300

    Update server.ts

--- rathudan-panel/lib/supabase/server.ts
+++ rathudan-panel/lib/supabase/server.ts
@@ -12,13 +12,13 @@ export function createClient() {
         getAll() {
           return cookieStore.getAll()
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: any[]) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) =>
+            cookiesToSet.forEach(({ name, value, options }: any) =>
               cookieStore.set(name, value, options)
             )
           } catch {
-            // Server Component - cookie setting ignored
+            // Server Component
           }
         },
       },
